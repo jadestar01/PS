e2f359aa2f4516a286677ff86909903296562239
jadestar01 <104671580+jadestar01@users.noreply.github.com>
2025-03-27 18:57:57 +0900

    [Bronze IV] Title: 빠른 A+B, Time: 224 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/15552. 빠른 A＋B/빠른 A＋B.cc
@@ -0,0 +1,22 @@
+#include <string>
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    string input = "";
+    long long cnt = 0;
+    cin >> input; cnt = stoi(input);
+
+    for (int i = 0 ; i < cnt; i++)
+    {
+        int x = 0; int y = 0;
+
+        cin >> input; x = stoi(input);
+        cin >> input; y = stoi(input);
+        cout << x + y << '\n';
+    }
+}
