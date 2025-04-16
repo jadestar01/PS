b9e683997990ea8b6e40718b008d4bc03ac3c548
jadestar01 <104671580+jadestar01@users.noreply.github.com>
2025-04-16 23:01:35 +0900

    [Bronze V] Title: 고양이, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10171. 고양이/고양이.cc
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    cout << "\\    /\\\n"
+         << " )  ( ')\n"
+         << "(  /  ) \n"
+         << " \\(__)|";
+
+    return 0;
+}
