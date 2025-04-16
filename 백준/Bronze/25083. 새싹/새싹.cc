17fa1addd92e563def23a8746a656a1ce08970bd
jadestar01 <104671580+jadestar01@users.noreply.github.com>
2025-04-16 23:10:10 +0900

    [Bronze V] Title: 새싹, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.cc
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    cout << "         ,r'\"7 \n"
+         << "r`-_   ,'  ,/\n"
+         << " \\. \". L_r\'\n"
+         << "   `~\\/\n"
+         << "      |\n"
+         << "      |\n";
+
+
+
+    return 0;
+}
