0590e954ebf2b4299eb3e79f72b246d1aa5c413e
jadestar01 <104671580+jadestar01@users.noreply.github.com>
2025-04-16 23:07:16 +0900

    [Bronze V] Title: 아스키 코드, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.cc
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    char a; cin >> a;
+
+    cout << (int)a;
+
+    return 0;
+}
