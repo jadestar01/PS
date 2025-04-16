d4d906b6b908088ff63082e811e1c7caf0afe69b
jadestar01 <104671580+jadestar01@users.noreply.github.com>
2025-04-16 23:05:44 +0900

    [Bronze V] Title: A×B, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10998. A×B/A×B.cc
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int a, b; cin >> a >> b;
+
+    cout 
+         << a * b <<"\n";
+
+    return 0;
+}
