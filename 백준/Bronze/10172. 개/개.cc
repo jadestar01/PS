d373ab3b46f46bd52ca9f84b0aeebcdf17cbe93c
jadestar01 <104671580+jadestar01@users.noreply.github.com>
2025-04-16 23:03:42 +0900

    [Bronze V] Title: 개, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10172. 개/개.cc
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    cout << "|\\_/|\n"
+         << "|q p|   /}\n"
+         << "( 0 )\"\"\"\\\n"
+         << "|\"^\"`    |\n"
+         << "||_/=\\\\__|";
+
+    return 0;
+}
