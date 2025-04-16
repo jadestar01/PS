794f983fd05100f134911f9caf9956a67041d3e5
jadestar01 <104671580+jadestar01@users.noreply.github.com>
2025-04-16 23:05:10 +0900

    [Bronze V] Title: 사칙연산, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10869. 사칙연산/사칙연산.cc
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int a, b; cin >> a >> b;
+
+    cout << a + b <<"\n"
+         << a - b <<"\n"
+         << a * b <<"\n"
+         << a / b <<"\n"
+         << a % b <<"\n";
+
+    return 0;
+}
