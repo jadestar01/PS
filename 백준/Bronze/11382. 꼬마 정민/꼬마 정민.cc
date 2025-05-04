e7f608b2bb9acc67737e3825f3798986564136dc
jadestar01 <104671580+jadestar01@users.noreply.github.com>
2025-05-05 00:02:33 +0900

    [Bronze V] Title: 꼬마 정민, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    long long int  x, y, Z;
+    cin >> x >> y >> Z;
+
+cout << x + y + Z;
+
+    return 0;
+}
