f704a2de2aae6c247640852fd25c9d7c633d4131
jadestar01 <104671580+jadestar01@users.noreply.github.com>
2025-05-05 18:19:51 +0900

    [Bronze V] Title: ??!, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.cc
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <math.h>
+#include <unordered_set>
+
+using namespace std;
+
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    string str; cin >> str;
+    cout << str << "??!";
+
+    return 0;
+}
