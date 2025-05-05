fe9133dc3ac5eb907b8e859d5afaaad6e1d10d19
jadestar01 <104671580+jadestar01@users.noreply.github.com>
2025-05-05 18:21:21 +0900

    [Bronze V] Title: 1998년생인 내가 태국에서는 2541년생?!, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/18108. 1998년생인 내가 태국에서는 2541년생？！/1998년생인 내가 태국에서는 2541년생？！.cc
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <math.h>
+#include <unordered_set>
+
+using namespace std;
+
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int y; cin >> y;
+    cout << y - 543;
+
+    return 0;
+}
